bcc47565224e8603577d9f98219742eacd7591f6
Levi GIdeon <contact@levigideon.com>
2012-01-09 08:42:14 -0600

    Moved wp_footer tag to correct location

--- Skimpy/footer.php
+++ Skimpy/footer.php
@@ -4,7 +4,7 @@
  */
 ?><br class="clr" /><!-- Cleans up the page, delete at your own peril. -->
 	<p><?php bloginfo( 'name' ); ?> || <a href="http://wordpress.org/" title="Powered By Wordpress" rel="generator"><?php printf( __( 'Running on %s.' ), 'WordPress' ); ?></a>
-	<?php wp_footer(); ?><!-- Keep this, plugins use it to hook and execute code -->
 	</div>
+	<?php wp_footer(); ?><!-- Keep this, plugins use it to hook and execute code -->
 </body>
 </html>
